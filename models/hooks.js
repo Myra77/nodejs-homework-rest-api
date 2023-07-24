a4bf2fefa22989622e78292fc91538f6a465665b
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-07-24 19:36:38 +0200

    create handleUpdateValidate

--- models/hooks.js
+++ models/hooks.js
@@ -1,4 +1,9 @@
-const handleSaveError = (error, data, next) => {
+export const handleSaveError = (error, data, next) => {
     error.status = 400;
     next();
-  };
+};
+
+export const handleUpdateValidate = function (next) {
+    this.options.runValidators = true;
+    next();
+};

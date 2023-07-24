0af24f57fa203860df87dc602ce235634243ff79
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-07-24 19:35:20 +0200

    adding process.exit & helpful message

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ import { app } from "./app.js";
 
 dotenv.config();
 
-const {DB_HOST, PORT} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`)
+      console.log("Database connection successful")
     })
   })
-  .catch(error => console.log(error.message));
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1);
+  });
 
 

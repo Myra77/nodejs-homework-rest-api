6b9b66a0ac66ee2a5234e5331d6d73432f8ae115
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-07-24 20:56:34 +0200

    update isValidId

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,11 +1,9 @@
 import { isValidObjectId } from "mongoose";
 import { HttpError } from "../helpers/index.js";
 
-const isValidId = (req, res, next) => {
+export const isValidId = (req, res, next) => {
     const { contactId } = req.params;
     if (!isValidObjectId(contactId))
     next(HttpError(400, `${contactId} is not valid id`));
     next();
 };
-
-export default isValidId;

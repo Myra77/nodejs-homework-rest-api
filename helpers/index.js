a60f6c91bf02cdfda3983165924828049a41b8b0
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-08-09 00:22:45 +0200

    save avatars to cloudinary

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 export { HttpError} from "./HttpError.js";
 export { addSchema } from "./schema.js";
 export { contactUpdateFavoriteSchema } from "./schema.js";
+export  { cloudinary}  from "./cloudinary.js";

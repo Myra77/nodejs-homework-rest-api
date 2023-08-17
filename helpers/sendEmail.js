6659ce2b6e90505ddc24faacff088c68c328b23a
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-08-17 14:40:24 +0200

    last updates

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,19 +2,19 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { EMAIL_PASSWORD, EMAIL_ADDRESS } = process.env;
+const { UKR_EMAIL_PASSWORD, UKR_EMAIL_ADDRESS } = process.env;
 
 const nodemailerConfig = {
     host: "smtp.ukr.net",
     port: 465,
     secure: true,
-    auth: { user: EMAIL_ADDRESS, pass: EMAIL_PASSWORD },
+    auth: { user: UKR_EMAIL_ADDRESS, pass: UKR_EMAIL_PASSWORD },
 };
 
 export const transport = nodemailer.createTransport(nodemailerConfig);
 
 export const sendEmail = async (data) => {
-    const email = { ...data, from: EMAIL_ADDRESS };
+    const email = { ...data, from: UKR_EMAIL_ADDRESS };
 
     await transport.sendMail(email);
     return true;
